ce3cfcbb1686c26662333a537ca2455b776b40e2
Anastasiya <soidberg91@gmail.com>
2023-04-28 20:26:21 +0300

    Add new HW API

---
+++ src/test/java/ReqresInTest.java
@@ -0,0 +1,92 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+import static org.hamcrest.Matchers.is;
+
+
+public class ReqresInTest {
+
+    @Test
+    void checkWithGivenTotal() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12))
+                //     .body("data[0].last_name",hasKey("Lawson"))
+                .extract().path("total");
+    }
+
+    @Test
+    void checkCreateNewUser() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+
+    @Test
+    void checkUpdateUser() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void checkDeleteUser() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    void checkLoginUser() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+}
+
